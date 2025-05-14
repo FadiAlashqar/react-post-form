47c73688641e11f67c04d6fe157dcf1d88ee06c4
FadiAlashqar <fadialshqar.fa01@gmail.com>
2025-05-14 19:49:38 +0200

    [UPD] useState asssigned

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,16 @@ import { useState } from 'react'
 
 function App() {
 
+  const [formData, setFormData] = useState({
+    author: "",
+    title: "",
+    body: "",
+    public: false
+
+
+
+
+  })
 
   return (
     <>
@@ -14,29 +24,32 @@ function App() {
         </div>
         <iv className="row">
           <div className="col-12">
-            <form className='bg-light mt-3'>
+            <form className='bg-light mt-3 p-4'>
               <div className="col-12">
                 <label for="author " class="form-label">Author</label>
-                <input name="author" type="text" class="form-control" id="author"></input>
+                <input value={formData.author} name="author" type="text" class="form-control" id="author"></input>
               </div>
               <div className="col-12">
                 <label for="title" class="form-label">Title</label>
-                <input name="title" type="text" class="form-control" id="title"></input>
+                <input value={formData.title} name="title" type="text" class="form-control" id="title"></input>
               </div>
               <div className="col-12">
                 <label for="body ">Body</label>
-                <textarea name="body" class="form-control" placeholder="Text goes here..." id="body"></textarea>
+                <textarea value={formData.body} name="body" class="form-control" placeholder="Text goes here..." id="body"></textarea>
               </div>
               <div className="col-12">
                 <label class="form-check-label" for="checkDefault">Do you want the post to be public ?</label>
                 <div class="form-check">
-                  <input name="public" class="form-check-input" type="checkbox" value="" id="public" />
+                  <input checked={formData.public} name="public" class="form-check-input" type="checkbox" value="Yes" id="public" />
                   <label class="form-check-label" for="public"> Yes </label>
                 </div>
                 <div class="form-check">
-                  <input name="public" class="form-check-input" type="checkbox" value="" id="public" />
+                  <input checked={formData.public} name="public" class="form-check-input" type="checkbox" value="No" id="public" />
                   <label class="form-check-label" for="public"> No </label>
                 </div>
+              </div>
+              <div className="col-12 mt-3">
+                <button type="submit" class="btn btn-primary">Submit</button>
               </div>
             </form>
           </div>
