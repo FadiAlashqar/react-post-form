eeb750aeadb7c1b0bc8671bc2599e2b22a67f972
FadiAlashqar <fadialshqar.fa01@gmail.com>
2025-05-14 19:20:18 +0200

    [UPD] form structure done

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,47 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <h1>REACT POST FORM</h1>
+          </div>
+        </div>
+        <iv className="row">
+          <div className="col-12">
+            <form className='bg-light mt-3'>
+              <div className="col-12">
+                <label for="author " class="form-label">Author</label>
+                <input name="author" type="text" class="form-control" id="author"></input>
+              </div>
+              <div className="col-12">
+                <label for="title" class="form-label">Title</label>
+                <input name="title" type="text" class="form-control" id="title"></input>
+              </div>
+              <div className="col-12">
+                <label for="body ">Body</label>
+                <textarea name="body" class="form-control" placeholder="Text goes here..." id="body"></textarea>
+              </div>
+              <div className="col-12">
+                <label class="form-check-label" for="checkDefault">Do you want the post to be public ?</label>
+                <div class="form-check">
+                  <input name="public" class="form-check-input" type="checkbox" value="" id="public" />
+                  <label class="form-check-label" for="public"> Yes </label>
+                </div>
+                <div class="form-check">
+                  <input name="public" class="form-check-input" type="checkbox" value="" id="public" />
+                  <label class="form-check-label" for="public"> No </label>
+                </div>
+              </div>
+            </form>
+          </div>
+        </iv>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
